4720ce4d6279a5b8cbcbc11194edfc93c9466b7b
Felipe Carvalho Funck <funck.felipe@hotmail.com>
2020-04-04 16:01:26 -0300

    Created DTOs for JPA entities

---
+++ course/src/main/java/academy/devdojo/youtube/course/model/dto/SectionDTO.java
@@ -0,0 +1,15 @@
+package academy.devdojo.youtube.course.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SectionDTO {
+
+    private Long id;
+    private String name;
+
+}
